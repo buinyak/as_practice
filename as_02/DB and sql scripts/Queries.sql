cc1ca660da9a061c422a17d20025a0871315b80a
buinyy <georgebui@yandex.ru>
2021-08-11 18:58:15 +0300

    БД и sql скрипты

--- as_02/DB and sql scripts/Queries.sql
+++ as_02/DB and sql scripts/Queries.sql
@@ -1,36 +1,53 @@
 DROP TABLE Staffs;
 DROP TABLE Departments;
 CREATE TABLE Departments (
-  id INT IDENTITY(1,1) PRIMARY KEY,
-  name VARCHAR(100)
+  Id INT IDENTITY(1,1) PRIMARY KEY,
+  Name VARCHAR(100)
 );
 CREATE TABLE Staffs (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    departament_id INT,
-    fio VARCHAR(100),
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Department_id INT,
+    Fio VARCHAR(100),
 	Salary MONEY,
-	FOREIGN KEY (departament_id) REFERENCES Departments(id)
+	FOREIGN KEY (Department_id) REFERENCES Departments(id) ON DELETE CASCADE,
+	
 );
-INSERT INTO Departments (name)
-    VALUES('HR');
-INSERT INTO Staffs (departament_id,fio,	Salary)
-    VALUES
-	(scope_identity(),'Andrei Andreev Andrevich',1000),
-	(scope_identity(),'Kirill Andreev Andrevich',2000),
-	(scope_identity(),'Sergei Andreev Andrevich',3000)
+
+DECLARE @id int,
+		@name VARCHAR(100);
+
+SET @name = 'HR';
 
 INSERT INTO Departments (name)
-    VALUES('Development');
-INSERT INTO Staffs (departament_id,fio,	Salary)
+    VALUES(@name);
+
+SET @id = (SELECT Departments.Id FROM Departments WHERE Departments.Name = @name);
+
+INSERT INTO Staffs (Department_id,Fio,	Salary)
     VALUES
-	(scope_identity(),'Misha Andreev Andrevich',43131),
-	(scope_identity(),'Kolya Andreev Andrevich',2003410),
-	(scope_identity(),'Nikita Andreev Andrevich',41313);
-SELECT TOP (1000) [id]
-      ,[name]
+	(@id,'Andrei Andreev Andrevich',1000),
+	(@id,'Kirill Andreev Andrevich',2000),
+	(@id,'Sergei Andreev Andrevich',3000)
+
+SET @name = 'Development';
+
+INSERT INTO Departments (name)
+    VALUES(@name);
+
+SET @id = (SELECT Departments.Id FROM Departments WHERE Departments.Name = @name);
+
+INSERT INTO Staffs (Department_id,Fio,	Salary)
+    VALUES
+	(@id,'Misha Andreev Andrevich',43131),
+	(@id,'Kolya Andreev Andrevich',2003410),
+	(@id,'Nikita Andreev Andrevich',41313);
+
+
+SELECT TOP (1000) [Id]
+      ,[Name]
 	  FROM [AnReshProbation].[dbo].[Departments]
-SELECT TOP (100) [id]
-      ,[departament_id]
-      ,[fio]
+SELECT TOP (100) [Id]
+      ,[Department_id]
+      ,[Fio]
       ,[Salary]
 	  FROM [AnReshProbation].[dbo].[Staffs]
